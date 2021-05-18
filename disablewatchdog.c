f9372b54998b16b9f1c3467411ef190134bae545
Artem Lomakin <40364948+mayerdev@users.noreply.github.com>
2021-05-18 15:17:22 +0300

    Update disablewatchdog.c

--- disablewatchdog.c
+++ disablewatchdog.c
@@ -2,6 +2,8 @@
 
 // This example shows how to bypass automatical exit
 // from application after 5 minutes
+// USE ONLY IF THE BOOTLOADER IS FULLY INITIALIZED AND READY TO RUN THE OS
+
 int main(int argc, char **argv) {
     (void) argc;
     (void) argv;
